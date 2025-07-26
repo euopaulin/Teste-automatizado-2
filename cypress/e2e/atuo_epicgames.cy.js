3c9b40353867af6062aa24f26722dc771d453dd9
Paulo Henrique <157616381+euopaulin@users.noreply.github.com>
2025-07-26 17:24:03 -0300

    Update

--- cypress/e2e/atuo_epicgames.cy.js
+++ cypress/e2e/atuo_epicgames.cy.js
@@ -1,12 +1,28 @@
 describe('Teste automatizado Epic Games', () => {
+
   it('Acessar o site', () => {
-    cy.visit('https://store.epicgames.com/pt-BR/', {
-  headers: {
-    'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
-  },
-  failOnStatusCode: false
-});
-    cy.get('#nav-flyout-toggle').click();
+    cy.visit('https://qaplayground.dev/');
+    cy.contains('Verify Your Account').click();
+
+    cy.get('.code-container > :nth-child(1)').type('9');
+    cy.get('.code-container > :nth-child(2)').type('9');
+    cy.get('.code-container > :nth-child(3)').type('9');
+    cy.get('.code-container > :nth-child(4)').type('9');
+    cy.get('.code-container > :nth-child(5)').type('9');
+    cy.get('.code-container > :nth-child(6)').type('9');
+
+    cy.get('.info').should('be.visible');
+
+    cy.get('.info').click();
   });
+
+  it('Acessar o site novamente', () => {
+    cy.visit('https://qaplayground.dev/apps/verify-account/');
+
+    cy.get('.topnav-links > .item').click();
+
+    cy.get('span').contains('QA Playground').should('be.visible');
+
+  });
+
 });
